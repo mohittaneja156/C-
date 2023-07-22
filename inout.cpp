4e569fabe6e8807b77faa8dcb198169c800edf7c
Mohit Taneja <mohittaneja156@gmail.com>
2023-07-22 14:56:50 +0530

    input output program

---
+++ inout.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter first number "<<endl;
+    cin>>a;
+    cout<<"Enter second number "<<endl;
+    cin>>b;
+    cout<<"sum of the numbers is"<<a+b<<endl;
+
+
+    return 0;
+}
